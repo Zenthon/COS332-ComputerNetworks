4d563240d250ad834cf882d2b8131b6387f31f83
Zenthon <u20536951@tuks.co.za>
2022-03-30 16:44:05 +0200

    Made changes to Server.java - Started the server and listen for clients

--- src/Server.java
+++ src/Server.java
@@ -1,2 +1,24 @@
+import java.net.*;
+import java.io.*;
+
 public class Server {
+
+    public static int client_number = 0;
+
+    public static void main(String[] args) throws Exception {
+        ServerSocket serverSocket = new ServerSocket(55555);
+        System.out.println("Server running on : " + 55555);
+
+        do {
+            Socket socket = serverSocket.accept();
+            System.out.println("Client [#" + client_number + " - " + socket.getInetAddress().getHostAddress() + "] connected!");
+
+            BufferedReader clientReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedWriter clientWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+            String request_line = clientReader.readLine();
+            System.out.println(request_line);
+
+        } while (true);
+    }
 }
