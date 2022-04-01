dba1c99db0bc7e77db9c4a84b50243bb9bcd068b
Zenthon <u20536951@tuks.co.za>
2022-04-01 14:07:16 +0200

    Check if server is accessed using GET method and HTTP/1.1

--- src/Server.java
+++ src/Server.java
@@ -6,8 +6,8 @@ public class Server {
     public static int client_number = 0;
 
     public static void main(String[] args) throws Exception {
-        ServerSocket serverSocket = new ServerSocket(55555);
-        System.out.println("Server running on : " + 55555);
+        ServerSocket serverSocket = new ServerSocket(51515);
+        System.out.println("Server running on : " + 51515);
 
         do {
             Socket socket = serverSocket.accept();
@@ -16,8 +16,11 @@ public class Server {
             BufferedReader clientReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             BufferedWriter clientWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
-            String request_line = clientReader.readLine();
-            System.out.println(request_line);
+            String reqLine = clientReader.readLine();
+            String[] reqLineArr = reqLine.split(" ");
+            if (reqLineArr[0].equals("GET") && reqLineArr[2].equals("HTTP/1.1")) {
+                System.out.println("Valid");
+            }
 
         } while (true);
     }
