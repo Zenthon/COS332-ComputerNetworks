e1355e9643a12346c025536d150bf1a500ba124c
Zenthon <u20536951@tuks.co.za>
2022-04-03 21:15:46 +0200

    Added a HTTPHandler and  made major changes to HTTPServer class

---
+++ src/HTTPClientHandler.java
@@ -0,0 +1,143 @@
+//  Isheanesu Joseph Dzingirai - u20536951
+//  Muziwandile Ndlovu - u20469366
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import java.io.*;
+import java.net.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.StringTokenizer;
+
+public class HTTPClientHandler extends Thread {
+    private Socket socket;
+    private BufferedReader clientReader;
+    private OutputStream clientWriter;
+    private static String answer = "0", response ="", expression ="";
+
+    public HTTPClientHandler(Socket s) throws IOException {
+        socket = s;
+        clientReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        clientWriter = socket.getOutputStream();
+    }
+
+    @Override
+    public void run() {
+        try {
+            ScriptEngineManager manager = new ScriptEngineManager();
+            ScriptEngine engine = manager.getEngineByName("js");
+
+            StringTokenizer parseInput = new StringTokenizer(clientReader.readLine());
+            String httpMethod = parseInput.nextToken().toUpperCase(), reqHTML = parseInput.nextToken().toUpperCase(), httpVersion = parseInput.nextToken().toUpperCase();
+            System.out.println(reqHTML);
+            if (!httpMethod.equals("GET") && !httpMethod.equals("HEAD")) {
+                String message = "<!DOCTYPE html>\n" +
+                                    "<html lang=\"en\">\n" +
+                                    "<head>\n" +
+                                    "    <meta charset=\"UTF-8\">\n" +
+                                    "    <title>Error 501</title>\n" +
+                                    "</head>\n" +
+                                    "<body>\n" +
+                                    "  Only HEAD and GET Requests are allowed\n" +
+                                    "</body>\n" +
+                                    "</html>";
+                String response = getStatusLine(501, "Not Implemented") + getHeaders(message.length()) + message;
+                clientWriter.write(response.getBytes());
+            } else if (!httpVersion.equals("HTTP/1.1")) {
+                String message = "<!DOCTYPE html>\n" +
+                        "<html lang=\"en\">\n" +
+                        "<head>\n" +
+                        "    <meta charset=\"UTF-8\">\n" +
+                        "    <title>Error 505</title>\n" +
+                        "</head>\n" +
+                        "<body>\n" +
+                        "  Only HTTP 1.1 Is allowed\n" +
+                        "</body>\n" +
+                        "</html>";
+                String response = getStatusLine(505, "HTTP Version Not Supported") + getHeaders(message.length()) + message;
+                clientWriter.write(response.getBytes());
+            } else {
+                if (reqHTML.equals("/"))
+                    response = getStatusLine(200, "OK") + getHeaders(getCalulator().length()) + getCalulator();
+                else if (reqHTML.equals("/C")){
+                    answer = "0";
+                    expression = "";
+                    response = getStatusLine(200, "OK") + getHeaders(getCalulator().length()) + getCalulator();
+                } else if (reqHTML.equals("/=")) {
+                    answer = String.valueOf(engine.eval(expression));
+                    response = answer.equals("INFINITY") ?
+                                getStatusLine(400, "Bad Request") + getHeaders(getCalulator().length() + 32) + "<p>Division by zero not allowed." + getCalulator() :
+                                getStatusLine(200, "OK") + getHeaders(getCalulator().length()) + getCalulator();
+                }
+                else {
+                    if (!reqHTML.equals("/div"))
+                        expression += reqHTML.charAt(1);
+                    else expression += "/";
+                    answer = expression;
+                    response = getStatusLine(200, "OK") + getHeaders(getCalulator().length()) + getCalulator();
+                }
+                clientWriter.write(response.getBytes());
+            }
+        } catch (IOException | ScriptException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public  String getStatusLine(int statusCode, String reasonPhrase) {
+        return "HTTP/1.1 " + statusCode + " " + reasonPhrase + "\r\n";
+    }
+
+    public String getHeaders(int contentLength) {
+        LocalDateTime currentDate = LocalDateTime.now();
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("E, MM dd yyyy HH:mm:ss");
+        return  "Server: Muzi and Ishe (WSL)\r\n" +
+                "Allow: GET, HEAD\r\n" +
+                "Date: " + currentDate.format(dateFormatter) + " GMT\r\n" +
+                "Server: Muzi and Ishe's Server\r\n" +
+                "Content-Type: text/html\r\n" +
+                "Content-Length: " + contentLength + "\r\n" +
+                "\r\n";
+
+    }
+
+    public String getCalulator() {
+        return "<!DOCTYPE html>\n" +
+                "<html lang=\"en\">\n" +
+                "<head>\n" +
+                "   <title>Calculator</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "   <table border=\"1\">\n" +
+                "       <tr>\n" +
+                "           <p><th colspan=\"4\">" + answer + "</th><p>\n" +
+                "       </tr>\n" +
+                "       <tr>\n" +
+                "           <td><a href =\"/1\"><button>1</button></a></td>\n" +
+                "           <td><a href =\"/2\"><button>2</button></a></td>\n" +
+                "           <td><a href =\"/3\"><button>3</button></a></td>\n" +
+                "           <td><a href =\"/+\"><button>+</button></a></td>\n" +
+                "       </tr>\n" +
+                "       <tr>\n" +
+                "           <td><a href =\"/4\"><button>4</button></a></td>\n" +
+                "           <td><a href =\"/5\"><button>5</button></a></td>\n" +
+                "           <td><a href =\"/6\"><button>6</button></a></td>\n" +
+                "           <td><a href =\"/-\"><button>-</button></a></td>\n" +
+                "       </tr>\n" +
+                "       <tr>\n" +
+                "           <td><a href =\"/7\"><button>7</button></a></td>\n" +
+                "           <td><a href =\"/8\"><button>8</button></a></td>\n" +
+                "           <td><a href =\"/9\"><button>9</button></a></td>\n" +
+                "           <td><a href =\"/*\"><button>*</button></a></td>\n" +
+                "       </tr>\n" +
+                "       <tr>\n" +
+                "           <td><a href =\"/0\"><button>0</button></a></td>\n" +
+                "           <td><a href =\"/C\"><button>C</button></a></td>\n" +
+                "           <td><a href =\"/=\"><button>=</button></a></td>\n" +
+                "           <td><a href =\"/div'\"><button>/</button></a></td>\n" +
+                "       </tr>\n" +
+                "   </table>\n" +
+                "</body>\n" +
+                "</html>";
+    }
+}
---
+++ src/HTTPServer.java
@@ -0,0 +1,127 @@
+//public class Server {
+//    public static int client_number = 0;
+//    public static BufferedReader clientReader = null;
+//    public static OutputStream clientWriter = null;
+//    public static String answer = "0";
+//    public static String expression = "";
+//
+//    public static void main(String[] args) throws Exception {
+//        ServerSocket serverSocket = new ServerSocket(51515);
+//        System.out.println("Server running on : " + 51515);
+//
+//        ScriptEngineManager manager = new ScriptEngineManager();
+//        ScriptEngine engine = manager.getEngineByName("js");
+//
+//        while (true){
+//            Socket socket = serverSocket.accept();
+//            clientReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//            clientWriter = socket.getOutputStream();
+//
+//            String reqLine = clientReader.readLine(),  response = "";
+//            String[] reqLineArr = reqLine.split(" ");
+//            if (reqLineArr[0].equals("GET") && reqLineArr[2].equals("HTTP/1.1")) {
+//               if (reqLineArr[1].equals("/")) {
+//                   response = getStatusLine(200, "OK") + getHeaders() + "\r\n";
+//                   clientWriter.write(response.getBytes());
+//               }
+//               else {
+//                   if (reqLineArr[1].equals("/=")) {
+//                       answer = String.valueOf(engine.eval(expression));
+//                       if (answer.equals("Infinity"))
+//                           response  = getStatusLine(400, "Bad Request") + getHeaders() + "\r\n";
+//                       else response = response = getStatusLine(200, "OK") + getHeaders() + "\r\n";
+//                   }
+//                   else if (reqLineArr[1].equals("/C")){
+//                       response = getStatusLine(200, "OK") + getHeaders() + "\r\n";
+//                       answer = expression = "0";
+//                   }
+//                   else {
+//                       response = getStatusLine(200, "OK") + getHeaders() + "\r\n";
+//                       if (!reqLineArr[1].equals("/div"))
+//                           expression += reqLineArr[1].charAt(1);
+//                       else expression += "/";
+//                       answer = expression;
+//                   }
+//                   clientWriter.write(response.getBytes());
+//               }
+//
+//               displayCalculator();
+//            }
+//        }
+//    }
+//
+
+//
+//    public static String getHeaders() {
+//        LocalDateTime currentDate = LocalDateTime.now();
+//        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("E, MM dd yyyy HH:mm:ss");
+//        return "Date: " + currentDate.format(dateFormatter) + " GMT\r\n" + "Server: Muzi and Ishe's Server\r\n" + "Content-Type: text/html\r\n" + "Content-Length: 1000\r\n";
+//    }
+//
+//    public static void displayCalculator() throws IOException {
+//        clientWriter.write("<!DOCTYPE html>".getBytes());
+//        clientWriter.write("<html lang=\"en\">".getBytes());
+//        clientWriter.write("<head>".getBytes());
+//            clientWriter.write("<title>Calculator</title>".getBytes());
+//        clientWriter.write("</head>".getBytes());
+//        clientWriter.write("<body>".getBytes());
+//
+//            clientWriter.write("<table border=\"1\">".getBytes());
+//                clientWriter.write("<tr>".getBytes());
+//                    clientWriter.write(("<p><th colspan=\"4\">" + answer + "</th><p>").getBytes());
+//                clientWriter.write("</tr>".getBytes());
+//                clientWriter.write("<tr>".getBytes());
+//                    clientWriter.write("<td><a href =\"/1\"><button>1</button></a></td>".getBytes());
+//                    clientWriter.write("<td><a href =\"/2\"><button>2</button></a></td>".getBytes());
+//                    clientWriter.write("<td><a href =\"/3\"><button>3</button></a></td>".getBytes());
+//                    clientWriter.write("<td><a href =\"/+\"><button>+</button></a></td>".getBytes());
+//                clientWriter.write("</tr>".getBytes());
+//                clientWriter.write("<tr>".getBytes());
+//                    clientWriter.write("<td><a href =\"/4\"><button>4</button></a></td>".getBytes());
+//                    clientWriter.write("<td><a href =\"/5\"><button>5</button></a></td>".getBytes());
+//                    clientWriter.write("<td><a href =\"/6\"><button>6</button></a></td>".getBytes());
+//                    clientWriter.write("<td><a href =\"/-\"><button>-</button></a></td>".getBytes());
+//                clientWriter.write("</tr>".getBytes());
+//                clientWriter.write("<tr>".getBytes());
+//                    clientWriter.write("<td><a href =\"/7\"><button>7</button></a></td>".getBytes());
+//                    clientWriter.write("<td><a href =\"/8\"><button>8</button></a></td>".getBytes());
+//                    clientWriter.write("<td><a href =\"/9\"><button>9</button></a></td>".getBytes());
+//                    clientWriter.write("<td><a href =\"/*\"><button>*</button></a></td>".getBytes());
+//                clientWriter.write("</tr>".getBytes());
+//                clientWriter.write("<tr>".getBytes());
+//                    clientWriter.write("<td><a href =\"/0\"><button>0</button></a></td>".getBytes());
+//                    clientWriter.write("<td><a href =\"/C\"><button>C</button></a></td>".getBytes());
+//                    clientWriter.write("<td><a href =\"/=\"><button>=</button></a></td>".getBytes());
+//                    clientWriter.write("<td><a href =\"div\"><button>/</button></a></td>".getBytes());
+//                clientWriter.write("</tr>".getBytes());
+//            clientWriter.write("</table>".getBytes());
+//        clientWriter.write("</body>".getBytes());
+//        clientWriter.write("</html>".getBytes());
+//    }
+//}
+
+
+import java.io.*;
+import java.net.*;
+import java.util.Locale;
+import java.util.StringTokenizer;
+
+public class HTTPServer {
+    public static int client_number = 1;
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(51515);
+            System.out.println("Server running on : " + 51515);
+            System.out.println("Waiting for client....");
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("Client " + socket.getInetAddress().getHostAddress() + "] connected!");
+                new HTTPClientHandler(socket).start();
+            }
+        } catch (IOException e) {
+           e.printStackTrace();
+        }
+    }
+}
