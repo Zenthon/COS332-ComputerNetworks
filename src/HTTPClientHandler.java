e12ebf24ede0c77988cb2b6b95f363bb3f0a3288
Zenthon <u20536951@tuks.co.za>
2022-04-05 12:21:17 +0200

    Mades changes for favicon.ico

--- src/HTTPClientHandler.java
+++ src/HTTPClientHandler.java
@@ -1,32 +1,39 @@
 //  Isheanesu Joseph Dzingirai - u20536951
 //  Muziwandile Ndlovu - u20469366
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptException;
 import java.io.*;
 import java.net.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Stack;
 import java.util.StringTokenizer;
+
 
 public class HTTPClientHandler extends Thread {
     private Socket socket;
     private BufferedReader clientReader;
     private OutputStream clientWriter;
     private static String answer = "0", response ="", expression ="";
+    private ScriptEngine engine;
+
+
     public enum StatusCode{
         OK("HTTP/1.1 200 OK \r\n"),
         BadRequest("HTTP/1.1 400 Bad Request \r\n"),
         httpVersion("HTTP/1.1 505 HTTP Version Not Supported \r\n"),
         NotImplemented("HTTP/1.1 501 Not Implemented \r\n");
         private String res ; 
-        private StatusCode(String response){
+        StatusCode(String response){
             this.res = response;
         }
     }
-    public HTTPClientHandler(Socket s) throws IOException {
+
+    public HTTPClientHandler(Socket s, ScriptEngine engine) throws IOException {
         socket = s;
         clientReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         clientWriter = socket.getOutputStream();
+        this.engine = engine;
     }
 
     @Override
@@ -42,7 +49,7 @@ public class HTTPClientHandler extends Thread {
                                     "    <title>Error 501</title>\n" +
                                     "</head>\n" +
                                     "<body>\n" +
-                                    "  Only HEAD and GET Requests are allowed\n" +
+                                    "  Only HEAD and GET Requests are allowed.\n" +
                                     "</body>\n" +
                                     "</html>";
                 String response = StatusCode.NotImplemented.res + getHeaders(message.length()) + message;
@@ -60,33 +67,43 @@ public class HTTPClientHandler extends Thread {
                         "</html>";
                 String response = StatusCode.httpVersion.res + getHeaders(message.length()) + message;
                 clientWriter.write(response.getBytes());
-            } else {
-                if (reqHTML.equals("/"))
-                    response =  StatusCode.OK.res  + getHeaders(getCalculator().length()) + getCalculator();
+            } else if (reqHTML.equalsIgnoreCase("/favicon.ico")) {
+                response = StatusCode.OK.res + getHeaders(getCalculator().length()) + getCalculator();
+                clientWriter.write(response.getBytes());
+            }
+            else {
+                if (reqHTML.equals("/") )
+                    response = StatusCode.OK.res + getHeaders(getCalculator().length()) + getCalculator();
                 else if (reqHTML.equals("/DEL")) {
-                    StringBuffer sb = new StringBuffer(expression);
-                    sb.deleteCharAt(sb.length()-1);
-                    expression = answer = sb.toString();
-                    response =StatusCode.OK.res + getHeaders(getCalculator().length()) + getCalculator();
+                    if (expression.length() != 0) {
+                        StringBuffer sb = new StringBuffer(expression);
+                        sb.deleteCharAt(sb.length() - 1);
+                        expression = answer = sb.toString();
+                        response = StatusCode.OK.res + getHeaders(getCalculator().length()) + getCalculator();
+                    }
                 }
 
-                else if (reqHTML.equals("/C")){
+                else if (reqHTML.equals("/AC")){
                     answer = "0";
                     expression = "";
                     response = StatusCode.OK.res + getHeaders(getCalculator().length()) + getCalculator();
                 } else if (reqHTML.equals("/=")) {
-                    answer = "" + eval(expression);
-                    response = answer.equalsIgnoreCase("INFINITY") ?
+                    answer = String.valueOf(Double.parseDouble(engine.eval(expression).toString()));
+                    if (answer.equals("-2147483648"))
+                        answer = "Error";
+                    expression = answer;
+                    response = answer.equals("Error") ?
                                 StatusCode.BadRequest.res + getHeaders(getCalculator().length() + 32) + "<center>Division by zero not allowed. Presss C to continue.<center>\n" + getCalculator() :
                                 StatusCode.OK.res + getHeaders(getCalculator().length()) + getCalculator();
-                    answer = "Error";
-
                 }
                 else {
-                    if (Character.isDigit(reqHTML.charAt(1)))
-                        expression += reqHTML.charAt(1);
+                    if (Character.isDigit(reqHTML.charAt(1)) || reqHTML.charAt(1) == '(' || reqHTML.charAt(1) == ')' || reqHTML.equals("/P") ) {
+                        if (reqHTML.equals("/P") && Character.isDigit(expression.charAt(expression.length()-1)))
+                            expression += ".";
+                        else expression += reqHTML.charAt(1);
+                    }
                     else {
-                        if (!Character.isDigit(expression.charAt(expression.length()-1))) {
+                        if (!Character.isDigit(expression.charAt(expression.length()-1)) && expression.charAt(expression.length()-1) != '(' && expression.charAt(expression.length()-1) != ')') {
                             StringBuffer sb = new StringBuffer(expression);
                             sb.deleteCharAt(sb.length()-1);
                             expression = sb.toString();
@@ -100,11 +117,15 @@ public class HTTPClientHandler extends Thread {
                     response = StatusCode.OK.res + getHeaders(getCalculator().length()) + getCalculator();
                 }
                 clientWriter.write(response.getBytes());
+                clientWriter.close();
+                clientReader.close();
+                socket.close();
             }
-        } catch (IOException e) {
+        } catch (IOException | ScriptException e) {
             e.printStackTrace();
         }
     }
+
     public String getHeaders(int contentLength) {
         LocalDateTime currentDate = LocalDateTime.now();
         DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("E, MM dd yyyy HH:mm:ss");
@@ -130,91 +151,37 @@ public class HTTPClientHandler extends Thread {
                 "           <p><th colspan=\"4\">" + answer + "</th><p>\n" +
                 "       </tr>\n" +
                 "       <tr>\n" +
-                "           <td><a href =\"/1\"><button>1</button></a></td>\n" +
-                "           <td><a href =\"/2\"><button>2</button></a></td>\n" +
-                "           <td><a href =\"/3\"><button>3</button></a></td>\n" +
-                "           <td><a href =\"/+\"><button>+</button></a></td>\n" +
+                "           <td><a href =\"/1\"><button style=\"width:100%\">1</button></a></td>\n" +
+                "           <td><a href =\"/2\"><button style=\"width:100%\">2</button></a></td>\n" +
+                "           <td><a href =\"/3\"><button style=\"width:100%\">3</button></a></td>\n" +
+                "           <td><a href =\"/+\"><button style=\"width:100%\">+</button></a></td>\n" +
                 "       </tr>\n" +
                 "       <tr>\n" +
-                "           <td><a href =\"/4\"><button>4</button></a></td>\n" +
-                "           <td><a href =\"/5\"><button>5</button></a></td>\n" +
-                "           <td><a href =\"/6\"><button>6</button></a></td>\n" +
-                "           <td><a href =\"/-\"><button>-</button></a></td>\n" +
+                "           <td><a href =\"/4\"><button style=\"width:100%\">4</button></a></td>\n" +
+                "           <td><a href =\"/5\"><button style=\"width:100%\">5</button></a></td>\n" +
+                "           <td><a href =\"/6\"><button style=\"width:100%\">6</button></a></td>\n" +
+                "           <td><a href =\"/-\"><button style=\"width:100%\">-</button></a></td>\n" +
                 "       </tr>\n" +
                 "       <tr>\n" +
-                "           <td><a href =\"/7\"><button>7</button></a></td>\n" +
-                "           <td><a href =\"/8\"><button>8</button></a></td>\n" +
-                "           <td><a href =\"/9\"><button>9</button></a></td>\n" +
-                "           <td><a href =\"/*\"><button>*</button></a></td>\n" +
+                "           <td><a href =\"/7\"><button style=\"width:100%\">7</button></a></td>\n" +
+                "           <td><a href =\"/8\"><button style=\"width:100%\">8</button></a></td>\n" +
+                "           <td><a href =\"/9\"><button style=\"width:100%\">9</button></a></td>\n" +
+                "           <td><a href =\"/*\"><button style=\"width:100%\">*</button></a></td>\n" +
                 "       </tr>\n" +
                 "       <tr>\n" +
-                "           <td><a href =\"/0\"><button>0</button></a></td>\n" +
-                "           <td><a href =\"/C\"><button>C</button></a></td>\n" +
-                "           <td><a href =\"/=\"><button>=</button></a></td>\n" +
-                "           <td><a href =\"/div\"><button>/</button></a></td>\n" +
+                "           <td><a href =\"/0\"><button style=\"width:100%\">0</button></a></td>\n" +
+                "           <td><a href =\"/AC\"><button style=\"width:100%\">AC</button></a></td>\n" +
+                "           <td><a href =\"/DEL\"><button style=\"width:100%\">DEL</button></a></td>\n" +
+                "           <td><a href =\"/div\"><button style=\"width:100%\">/</button></a></td>\n" +
                 "       </tr>\n" +
                 "       <tr>\n" +
-                "           <td align=\"center\" colspan=\"4\"><a href =\"/DEL\"><button style=\"width:100%\">DEL</button></a></td>\n" +
+                "           <td><a href =\"/(\"><button style=\"width:100%\">(</button></a></td>\n" +
+                "           <td><a href =\"/)\"><button style=\"width:100%\">)</button></a></td>\n" +
+                "           <td><a href =\"/P\"><button style=\"width:100%\">.</button></a></td>\n" +
+                "           <td><a href =\"/=\"><button style=\"width:100%\">=</button></a></td>\n" +
                 "       </tr>\n" +
                 "   </table>\n" +
                 "</center></body>\n" +
                 "</html>";
-    }
-
-    public int applyOperand(char operand, int b, int a) {
-        switch (operand) {
-            case '+':
-                return a + b;
-            case '-':
-                return a - b;
-            case '*':
-                return a * b;
-            case '/':
-                return (b == 0) ? Integer.MIN_VALUE : (a / b);
-        }
-        return 0;
-    }
-
-    public boolean hasPrecedence(char op1, char op2) {
-        if (op2 == '(' || op2 == ')')
-            return false;
-        if ((op1 == '*' || op1 == '/') && (op2 == '+' || op2 == '-'))
-            return false;
-        return true;
-    }
-
-    public int eval(String expression) {
-        char[] tokens = expression.toCharArray();
-        Stack<Integer> constants = new Stack<Integer>();
-        Stack<Character> operands = new Stack<Character>();
-
-        for (int i = 0; i < tokens.length; i++) {
-            if (tokens[i] == ' ')
-                continue;
-            if (tokens[i] >= '0' && tokens[i] <= '9') {
-                StringBuffer sbuf = new StringBuffer();
-                while (i < tokens.length && tokens[i] >= '0' && tokens[i] <= '9')
-                    sbuf.append(tokens[i++]);
-                constants.push(Integer.parseInt(sbuf.toString()));
-                i--;
-            }
-            else if (tokens[i] == '(')
-                operands.push(tokens[i]);
-            else if (tokens[i] == ')') {
-                while (operands.peek() != '(')
-                    constants.push(applyOperand(operands.pop(), constants.pop(), constants.pop()));
-                operands.pop();
-            }
-
-            else if (tokens[i] == '+' || tokens[i] == '-' || tokens[i] == '*' || tokens[i] == '/') {
-                while (!operands.empty() && hasPrecedence(tokens[i], operands.peek()))
-                    constants.push(applyOperand(operands.pop(), constants.pop(), constants.pop()));
-                operands.push(tokens[i]);
-            }
-        }
-
-        while (!operands.empty())
-            constants.push(applyOperand(operands.pop(), constants.pop(), constants.pop()));
-        return constants.pop();
     }
 }
--- src/HTTPServer.java
+++ src/HTTPServer.java
@@ -1,13 +1,18 @@
 //  Isheanesu Joseph Dzingirai - u20536951
 //  Muziwandile Ndlovu - u20469366
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
 import java.io.*;
 import java.net.*;
 import java.util.*;
 
 public class HTTPServer {
+    static ScriptEngineManager manager = new ScriptEngineManager();
+    static ScriptEngine engine = manager.getEngineByName("js");
 
     public static void main(String[] args) {
+        System.out.print("\033[H\033[2J");
         try {
             ServerSocket serverSocket = new ServerSocket(50505);
             System.out.println("Server running on : " + 50505);
@@ -15,7 +20,7 @@ public class HTTPServer {
 
             while (true) {
                 Socket socket = serverSocket.accept();
-                new HTTPClientHandler(socket).start();
+                new HTTPClientHandler(socket, engine).start();
             }
         } catch (IOException e) {
            e.printStackTrace();
