03f7d94d2f05d8d5ffc6736113e7cff4822ee7de
Zenthon <u20536951@tuks.co.za>
2022-05-07 14:48:24 +0200

    :sparkles: Added functionality to sense when door is open

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,15 @@
+
+import java.io.IOException;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("House Alarm System Activated. Sensors On!");
+        System.out.println("The Door is connected to the ENTER key. Opening the door will cause ENTER key to be triggered.");
+        System.out.println("Waiting for door to open........");
+
+        System.in.read();
+        System.out.println("Door has been opened. Sensors and alarm triggered.");
+
+
+    }
 }
