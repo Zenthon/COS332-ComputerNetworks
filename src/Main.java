579f4cfa074a3e79142b912437de863792821d24
Zenthon <u20536951@tuks.co.za>
2022-05-21 11:43:22 +0200

    :sparkles: :fire: Added the file watcher

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        // Track whether the file has been changed
+        TimerTask timerTask = new FileWatcher(new File("index.html")) {
+            @Override
+            protected void onChange(File file) {
+                System.out.println("File " + file.getName() + " has changed.");
+                // Upload the changes to the apache server
+
+            }
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(timerTask, new Date(), 1000);
+    }
 }
