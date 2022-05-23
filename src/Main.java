0730a33de5fccda6af008c9326076a102002fe6c
Zenthon <u20536951@tuks.co.za>
2022-05-23 02:07:38 +0200

    :sparkles: :fire: Practical 8 Complete. All functionality working

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,26 @@
+import java.net.UnknownHostException;
 import java.util.*;
 import java.io.*;
 
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException {
+        FTP ftp = new FTP("localhost");
+        String fileName = "index.html";
 
+        System.out.println("Watching: " + fileName);
         // Track whether the file has been changed
-        TimerTask timerTask = new FileWatcher(new File("index.html")) {
+        TimerTask timerTask = new FileWatcher(new File(fileName)) {
             @Override
             protected void onChange(File file) {
                 System.out.println("File " + file.getName() + " has changed.");
                 // Upload the changes to the apache server
-
+                try {
+                    ftp.upload(fileName);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         };
 
